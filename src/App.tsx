fd611ddf52b02c728130524b21a7f3b3fac304e1
viktorKorneev <korneevviktor90@yandex.ru>
2026-06-20 08:21:12 +0300

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function hello() {
+    debugger
+    alert("Hello IT-KAMASUTRA")
+}
+
+// hello();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+
+    )
+        ;
+}
+
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return <>This is App component</>
+}
+
+function Rating() {
+    console.log("Rating rendering");
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+function Star() {
+    console.log("Star rendering")
+    return <div>star</div>
+}
+
+function Accordion() {
+    console.log("Accordion rendering")
+    return <div>
+        <h3>Меню</h3>
     </div>
-  );
 }
 
 export default App;
